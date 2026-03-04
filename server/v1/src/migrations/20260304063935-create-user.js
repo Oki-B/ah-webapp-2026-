fed0a06f48b3ebf7b93138ca4b44c3036b2b76d8
Oki <biek.syaoki@gmail.com>
2026-03-04 13:56:40 +0700

    feat(server-v1-auth): init model and migration config of user table

---
+++ server/v1/src/migrations/20260304063935-create-user.js
@@ -0,0 +1,48 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.UUIDV4
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      is_email_verified: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      is_active: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      provider: {
+        type: Sequelize.STRING
+      },
+      provider_id: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
